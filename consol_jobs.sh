9f573e70cb86ec3b4d7c7b35ac6a798ae4dc2892
AWS ParallelCluster user <ec2-user@ip-10-0-13-43.ap-southeast-1.compute.internal>
2020-10-24 13:33:37 +0000

    Files for Lab 6

--- consol_jobs.sh
+++ consol_jobs.sh
@@ -7,8 +7,8 @@ cmd1="sbatch --dependency=afterok:"
 counter1=0
 for i in "${temp1[@]}"; do
        if [[ "$i" == "compute" ]]; then
-	       id1=${temp1[$counter1-1]}
-	       cmd1="${cmd1}${id1}:"
+               id1=${temp1[$counter1-1]}
+               cmd1="${cmd1}${id1}:"
        fi
        counter1=$((counter1+1))
 done
